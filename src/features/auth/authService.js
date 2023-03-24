b4cb3dc86b22953f99c23ddcab013be00dd700e2
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2023-03-24 23:58:24 +0700

    add pages: Home, Login, Register, Header

---
+++ src/features/auth/authService.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const
+API_URL='http://localhost:5000/api/v1/auth/register'
+//Register user
+const register = async (userData) => {
+    try{
+        const response = await axios.post(API_URL,userData)
+        console.log(JSON.stringify(response.data)) 
+        console.log(response.data.name) 
+    if(response.data) {
+            //localStorage.setItem('user',JSON.stringify(response.data))
+        localStorage.setItem('user',response.data.name)
+    }
+    return response.data.name//response.data
+    } 
+    catch(error) {
+        console.log('authService: register') 
+        console.log(error);
+    }
+}
+const authService ={
+    register,
+}
+export default authService;
